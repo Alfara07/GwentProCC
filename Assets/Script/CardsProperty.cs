ef421fcac0adadd07005586192f7334e6dd04425
Alfara07 <160906865+Alfara07@users.noreply.github.com>
2024-03-11 15:42:20 -0400

    Mi primer commit (se agregan las primeras cartas)

---
+++ Assets/Script/CardsProperty.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsProperty : MonoBehaviour
+{
+public string Name;
+public string Faction;
+public string Type; 
+}
---
+++ Assets/Script/Deck.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+public GameObject[] Decks = new GameObject[30];
+/*private void Shuffle() 
+{
+ for(Shuffle )   
+}*/
+}
