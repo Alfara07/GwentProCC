ca1c1fd628e012ad167b0cac0f7a88497f349aed
Alfara07 <160906865+Alfara07@users.noreply.github.com>
2024-03-13 01:47:52 -0400

    Se crearon los Deck y se desarrollo el script Deck
    1-Se crearon los GameObject Deck1 y GameObject Deck2
    2-Se creo el metodo para barajear las cartas de los mazos
    3-Se creo el metodo para instanciar las cartas en la mano
    4-Se crearon los GameObject Hand1 y GameObject Hand2
    5-Se crearon las posiciones provisionales de las cartas de ambos mazos

--- Assets/Script/Deck.cs
+++ Assets/Script/Deck.cs
@@ -4,9 +4,46 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-public GameObject[] Decks = new GameObject[30];
-/*private void Shuffle() 
-{
- for(Shuffle )   
-}*/
+ public GameObject[] Decks = new GameObject[30];
+ public GameObject[] Hands = new GameObject[10];
+ public GameObject[] Positions = new GameObject[10];
+
+ private int Position = 0;
+
+ //Metodo para llevar las cartas del mazo a la mano.
+ private void MASTER(int cant)
+ {
+    for(int x = 0; x < cant; x++)
+    {
+        for(int y = 0; y < Hands.Length; y++)
+        {
+            if(Hands[y] == null )
+            {
+                Hands[y] = GameObject.Instantiate(Decks[Position], Positions[y].transform.position, Positions[y].transform.rotation); //Spawnea las cartas en la mano.
+                Hands[y].transform.localScale = Positions[y].transform.localScale;
+                Position++;
+                break;
+            }
+        }
+
+    }
+ }
+
+ private void Start()
+ {
+    Shuffle();
+    MASTER(10);
+ }
+
+ //Metodo para barajear las cartas de forma aleatoria.
+ private void Shuffle() 
+ {
+   for(int x = 0; x < Decks.Length; x++)
+    {
+      GameObject card = Decks[x];
+      int r = Random.Range(0,Decks.Length);
+      Decks[x] = Decks[r];
+      Decks[r] = card;
+    }   
+ }
 }
