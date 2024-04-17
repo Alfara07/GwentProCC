cabf83abdb5a3aed092704f0dee5161029a8734f
Alfara07 <160906865+Alfara07@users.noreply.github.com>
2024-04-17 03:57:37 -0400

    Tablero, Cartas y Boton
    1-Se agregaron imagenes finales de cartas, tablero y fondo
    2-Se crearon todas las cartas definitivas de ambos Decks
    3-Se conformaron los Decks definitivos
    4-Se agrego un tablero a proporsion
    5-Se incluyo un fondo
    6-Se creo el GameObject "BigCard" para agrandar visualmente las cartas
    7-Se crea un boton para pasar de turnos
    8-Se crea un metodo para pasarle y quitarle al GameObject "BigCard" las
    cartas

--- Assets/Script/CardsProperty.cs
+++ Assets/Script/CardsProperty.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class CardsProperty : MonoBehaviour
 {
-public string Name;
-public string Faction;
-public string Type; 
+    public string Name;
+    public string Faction;
+    public string Type;
+
+    //Localizando el Big Card
+    private SpriteRenderer BigCard;
+
+    private void Start()
+    {
+        BigCard = GameObject.FindGameObjectWithTag("BigCard").GetComponent<SpriteRenderer>();
+    }
+
+    //Metodos para mostrar y dejar de mostrar las cartas en grande
+    private void OnMouseEnter()
+    {
+        BigCard.sprite=GetComponent<SpriteRenderer>().sprite;
+    }
+
+    private void OnMouseExit()
+    {
+        BigCard.sprite=null;
+    }
 }
